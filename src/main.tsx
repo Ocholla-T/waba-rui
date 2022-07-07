010da01f25f56f1c98da66254f813476106e8bad
Ocholla-T <tonnyocholla8@gmail.com>
2022-07-07 17:36:12 +0300

    ADD: routing to various parts of application

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
-import './index.css'
+import { Register } from '@pages/register'
+import './index.scss'
+import { Login } from '@pages/login'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="auth/register" element={<Register />} />
+        <Route path="auth/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
