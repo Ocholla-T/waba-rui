6f4b3f2aa8288f837dfdc8b18786a94fd72f8faa
Ocholla-T <tonnyocholla8@gmail.com>
2022-08-02 12:40:54 +0300

    moved entrance to app to App.tsc

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Dashboard } from '@pages/dashboard'
-import { Register } from '@pages/register'
+
 import './index.scss'
-import { Login } from '@pages/login'
-import { PrivateRoute } from '@utils/privateRoute'
+
+import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* <Route element={<PrivateRoute />}>
-          <Route path="/" element={<Dashboard />} />
-        </Route> */}
-        <Route path="auth/register" element={<Register />} />
-        {/* <Route path="auth/login" element={<Login />} /> */}
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
