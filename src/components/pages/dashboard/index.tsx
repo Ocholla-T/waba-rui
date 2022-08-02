490da50c645aa25a1d39fb7f38c24dc777b9883a
Ocholla-T <tonnyocholla8@gmail.com>
2022-08-02 12:41:25 +0300

    ADD added navigationbar component

--- src/components/pages/dashboard/index.tsx
+++ src/components/pages/dashboard/index.tsx
@@ -1,40 +1,13 @@
-import { FC, ReactElement } from 'react'
 import { NavigationBar } from '@ui/navigationBar'
-import { CustomButton } from '@ui/button'
-import './_styles.scss'
+import { FC, ReactElement } from 'react'
 
-type dashboardProps = {}
+type DashboardProps = {}
 
-export const Dashboard: FC<dashboardProps> = ({}: dashboardProps): ReactElement => {
+export const Dashboard: FC<DashboardProps> = ({}): ReactElement => {
   return (
-    <main>
+    <>
       <NavigationBar />
-      <section className="dashboard">
-        <div className="dashboard--header">
-          <h1>Houses (Rooms)</h1>
-          <CustomButton
-            type="button"
-            value="Add a house"
-            style={{ width: 'fit-content', textTransform: 'none' }}
-            className="container-override"
-          />
-        </div>
-        <div className="dashboard--content">
-          <table className="dashboard--content--table">
-            <thead>
-              <tr className="dashboard--content--table--header">
-                <th>#</th>
-                <th>House number</th>
-                <th>Tenant name</th>
-                <th>Tenant phone</th>
-                <th>Meter reading</th>
-                <th>Lease(Move-In)</th>
-                <th>Evict(Move-Out)</th>
-              </tr>
-            </thead>
-          </table>
-        </div>
-      </section>
-    </main>
+      <main></main>
+    </>
   )
 }
