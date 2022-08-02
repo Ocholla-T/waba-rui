8f2ccee3741a31268193569ef365672f985a02e1
Ocholla-T <tonnyocholla8@gmail.com>
2022-08-02 12:40:15 +0300

    Entrance point for entire app

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { FC, useState } from 'react'
+import { Onboarding } from '@pages/onboarding'
+import { Verification } from '@pages/verification'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Dashboard } from '@pages/dashboard'
+import { Register } from '@pages/register'
+import { Login } from '@pages/login'
+
+type AppProps = {}
+
+export const App: FC<AppProps> = ({}: AppProps) => {
+  const [isRegistering, setIsRegistering] = useState(false)
+
+  const toggleIsRegisteringState: (state: boolean) => void = (state: boolean) => {
+    setIsRegistering(state)
+  }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="auth/register"
+          element={<Register toggleIsRegisteringState={toggleIsRegisteringState} />}
+        />
+        <Route path="auth/login" element={<Login />} />
+        <Route path="onboarding" element={<Onboarding />} />
+        <Route
+          path="verification"
+          element={
+            <Verification
+              isRegistering={isRegistering}
+              toggleIsRegisteringState={toggleIsRegisteringState}
+            />
+          }
+        />
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
