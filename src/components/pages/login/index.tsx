a8df172961b5bec38b0694ece9115c5653163175
Ocholla-T <tonnyocholla8@gmail.com>
2022-08-02 12:42:17 +0300

    ADD: login form functionality and template

--- src/components/pages/login/index.tsx
+++ src/components/pages/login/index.tsx
@@ -1,74 +1,128 @@
-import axios from 'axios'
-import { ChangeEvent, FC, FormEvent, FormEventHandler, ReactElement, useState } from 'react'
-import { Link, NavigateFunction, useNavigate } from 'react-router-dom'
-import { CustomButton } from '@ui/button'
-import { CustomInput } from '@ui/input'
+import Logo from '@images/logo.png'
+import { Box, Button, Card, TextField } from '@mui/material'
 import { AuthService } from '@services/auth'
-import logo from '@images/logo.png'
-
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+import React, {
+  ChangeEvent,
+  ChangeEventHandler,
+  FC,
+  FormEvent,
+  FormEventHandler,
+  ReactElement,
+  useState,
+} from 'react'
+import { useNavigate } from 'react-router-dom'
 import './_index.scss'
 
-type loginProps = {}
+type LoginProps = {}
 
-export const Login: FC<loginProps> = ({}: loginProps): ReactElement => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const device_name = navigator.userAgent
-  const navigate: NavigateFunction = useNavigate()
+export const Login: FC<LoginProps> = ({}: LoginProps): ReactElement => {
+  const [inputValue, setInputValue] = useState({ phone: '' })
+  const navigate = useNavigate()
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (
-    event: FormEvent<HTMLFormElement>,
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (
+    event: ChangeEvent<HTMLInputElement>,
   ): void => {
+    const { value } = event.target
+
+    setInputValue({ phone: value })
+  }
+
+  const generateCode: FormEventHandler<HTMLDivElement> = async (
+    event: FormEvent<HTMLDivElement>,
+  ): Promise<void> => {
     event.preventDefault()
-    axios
-      .post('https://waba-api.rascan.co.ke/api/token/generate', { username, password, device_name })
-      .then((response) => {
-        if (response.status === 200) {
-          AuthService.setLocalStorage(response)
-          navigate('/')
-        }
+
+    const config: AxiosRequestConfig<any> = {
+      method: 'POST',
+      url: 'https://waba-api.rascan.co.ke/api/code/generate',
+      headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+      data: {
+        ...inputValue,
+      },
+    }
+
+    await axios
+      .request(config)
+      .then((response: AxiosResponse<any, any>) => {
+        localStorage.setItem(
+          'waba_caretaker',
+          JSON.stringify({ data: { user: { phone: inputValue.phone } } }),
+        )
+        navigate('/verification')
       })
       .catch((error) => {
-        console.error(error)
+        console.log(error)
       })
   }
 
-  const getUsername = (event: ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value)
-  }
-
-  const getPassword = (event: ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value)
-  }
   return (
-    <main className="register">
-      <form action="" method="post" className="login--form" onSubmit={handleSubmit}>
-        <img className="register--form--logo" src={logo} />
-        <div className="login--form--title">
-          <h2>Sign In</h2>
-          <p>Access your caretaker account</p>
-        </div>
-        <CustomInput
-          className="login--input"
-          id="username"
-          label="Username"
-          type="text"
-          autoComplete="email"
-          handleChange={getUsername}
-        />
-        <CustomInput
-          className="login--input"
-          id="password"
-          label="Password"
-          type="password"
-          autoComplete="newPassword"
-          handleChange={getPassword}
-        />
-        <CustomButton className="login--form--button" value="Log In" type="submit" />
-        <h3 className="login--form--option">
-          Don't have an account, <Link to="/auth/register">Register</Link>
-        </h3>
-      </form>
+    <main className="login">
+      <Box
+        sx={{
+          minWidth: '100vw',
+          minHeight: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Card
+          className="login-form"
+          variant="outlined"
+          component="form"
+          sx={{
+            display: 'flex',
+            width: { xs: '95%', sm: '30%' },
+            padding: '2rem ',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: '20px',
+          }}
+          onSubmit={generateCode}
+        >
+          <h1 className="login-form--title">WABA</h1>
+          <img className="login-form--image" src={Logo} alt="waba logo" />
+          <div className="login-form--text">
+            <h2 className="login-form--text__subtitle">Login</h2>
+            <p className="login-form--text__desc">Sign in to your caretaker account</p>
+          </div>
+
+          <TextField
+            className="login-form--input"
+            required
+            id="phone"
+            label="Phone number"
+            placeholder="+254 ********"
+            type="tel"
+            autoComplete="new-password"
+            fullWidth
+            size="small"
+            sx={{
+              marginTop: ' 1.1875rem',
+            }}
+            // error={errors.phone[0].length > 0}
+            // helperText={errors.phone[0].length > 0 && `${errors.phone[0]}`}
+            value={inputValue.phone}
+            onChange={handleChange}
+          />
+
+          <Button
+            type="submit"
+            variant="contained"
+            size="large"
+            sx={{
+              width: '100%',
+              marginTop: ' 1.875rem',
+              backgroundColor: '#00AAA7',
+            }}
+          >
+            Generate Code
+          </Button>
+        </Card>
+      </Box>
     </main>
   )
 }
