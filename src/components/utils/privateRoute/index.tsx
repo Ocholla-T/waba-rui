d4a47dfba5af907f0b39e7bf60064a12f3f02812
Ocholla-T <tonnyocholla8@gmail.com>
2022-08-02 12:52:59 +0300

    DELETE unused component

--- src/components/utils/privateRoute/index.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthService } from '@services/auth'
-import { FC, ReactElement } from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-
-type protectedRouteProps = {}
-
-export const PrivateRoute: FC<protectedRouteProps> = ({}: protectedRouteProps): ReactElement => {
-  return AuthService.isAuthenticated() ? <Outlet /> : <Navigate to="auth/login" />
-}
