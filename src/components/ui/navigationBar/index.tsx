89d1995cbe8806f053f1fc9f7104a41d707d38ed
Ocholla-T <tonnyocholla8@gmail.com>
2022-08-02 12:52:40 +0300

    ADD: navigation bar styling and templating

--- src/components/ui/navigationBar/index.tsx
+++ src/components/ui/navigationBar/index.tsx
@@ -1,17 +1,61 @@
 import { FC, ReactElement } from 'react'
-import LogoutIcon from '@images/logout.svg'
+
 import './_styles.scss'
+import { AppBar, Avatar, IconButton, Toolbar, Typography } from '@mui/material'
+import Logo from '@images/logo.png'
+import { AuthService } from '@services/auth'
+import LogoutIcon from '@mui/icons-material/PowerSettingsNewOutlined'
+import { cyan, pink } from '@mui/material/colors'
+import { useNavigate } from 'react-router-dom'
 
-type navigationBarProps = {}
+type NavigationBarProps = {}
 
-export const NavigationBar: FC<navigationBarProps> = ({}: navigationBarProps): ReactElement => {
+export const NavigationBar: FC<NavigationBarProps> = ({}): ReactElement => {
+  const navigate = useNavigate()
+  const name =
+    (AuthService.getLocalStorage().data.name as string) ??
+    (AuthService.getLocalStorage().data.apartment.name as string)
+
+  const logoutCaretaker: VoidFunction = (): void => {
+    AuthService.logout()
+    navigate('/auth/login')
+  }
+
   return (
-    <nav className="nav">
-      <h1 className="nav--logo">Waba</h1>
-      <div>
-        <div className="nav--icon">D</div>
-        <img className="nav--logout" src={LogoutIcon} alt="logout" />
-      </div>
-    </nav>
+    <AppBar color="inherit">
+      <Toolbar sx={{ justifyContent: 'space-between ' }}>
+        <div className="nav--title">
+          <img className="nav--title--logo" src={Logo} alt="waba logo" />
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{
+              display: { xs: 'none', sm: 'block' },
+              fontFamily: ['"Kalam", cursive'],
+              color: '#00aaa7',
+              fontWeight: 700,
+            }}
+          >
+            {name}
+          </Typography>
+        </div>
+        <div className="nav--profile_information">
+          <Avatar
+            sx={{
+              bgcolor: cyan[800],
+              fontSize: 14,
+              width: 34,
+              height: 34,
+            }}
+          >
+            {name.charAt(0)}
+          </Avatar>
+          <IconButton aria-label="logout" onClick={logoutCaretaker}>
+            <LogoutIcon fontSize="small" sx={{ color: pink[500] }} />
+          </IconButton>
+        </div>
+      </Toolbar>
+    </AppBar>
   )
 }
