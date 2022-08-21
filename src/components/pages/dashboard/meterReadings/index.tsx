ea935cef29c086351eac9061871e942a9acd0699
Ocholla-T <tonnyocholla8@gmail.com>
2022-08-21 12:03:21 +0300

    ADD: meter reading component

---
+++ src/components/pages/dashboard/meterReadings/index.tsx
@@ -0,0 +1,13 @@
+import { NavigationBar } from '@ui/navigationBar'
+import { FC } from 'react'
+
+type Props = {}
+
+export const MeterReadings: FC<Props> = ({}) => {
+  return (
+    <>
+      <NavigationBar />
+      <p>hello</p>
+    </>
+  )
+}
