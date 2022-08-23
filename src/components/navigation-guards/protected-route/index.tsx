6621dbe1f63251f01a2851b83396b0adb6f59549
Ocholla-T <tonnyocholla8@gmail.com>
2022-08-23 09:29:25 +0300

    ADD: protected route component for navigation guards

---
+++ src/components/navigation-guards/protected-route/index.tsx
@@ -0,0 +1,12 @@
+import { AuthService } from "@services/auth";
+import { FC } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+type Props = {};
+export const ProtectedRoute: FC<Props> = ({}) => {
+  const isAuthenticated = AuthService.isAuthenticated();
+
+  return (
+    <>{!isAuthenticated ? <Navigate to="/auth/login" replace /> : <Outlet />}</>
+  );
+};
