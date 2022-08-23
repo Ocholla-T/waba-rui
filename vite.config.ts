a8060e0db38e3eb5e16b6ab19a3872ef1b8e5539
Ocholla-T <tonnyocholla8@gmail.com>
2022-08-23 09:27:59 +0300

    ADD: path alias to components folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-import * as path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: {
-      '@ui': path.resolve(__dirname, './src/components/ui'),
-      '@pages': path.resolve(__dirname, './src/components/pages'),
-      '@images': path.resolve(__dirname, './src/assets/images'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@utils': path.resolve(__dirname, './src/components/utils'),
+      "@ui": path.resolve(__dirname, "./src/components/ui"),
+      "@pages": path.resolve(__dirname, "./src/components/pages"),
+      "@images": path.resolve(__dirname, "./src/assets/images"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@utils": path.resolve(__dirname, "./src/components/utils"),
+      "@components": path.resolve(__dirname, "./src/components"),
+
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
     },
   },
-})
+});
